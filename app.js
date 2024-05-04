2081a7c4732fe4e7e0c536aae5b22cba36fb9e76
Sajid <sajid.saddique@alquranclasses.com>
2024-05-04 17:02:01 +0500

    => db url updated

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const userRoutes = require('./routes/users');
 
 const mongoose = require('mongoose');
 //const dbURL = 'mongodb://127.0.0.1:27017/FootBallApp';
-const dbURL = 'mongodb+srv://sajid89sahal@gmail.com:' + encodeURIComponent('Sajid@1989') + '@cluster0.za1d0lw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbURL = 'mongodb+srv://sajid89sahal@gmail.com:' + encodeURIComponent('Sajid@1989') + '@cluster0.za1d0lw.mongodb.net/FootBallApp';
 
 app.use(express.json()); // Make sure it comes back as json
 app.use('/api/users', userRoutes); // Prefix all routes defined in `userRoutes` with `/api/users`
